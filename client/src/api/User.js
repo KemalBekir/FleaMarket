dacc284380eb9050fcc5427b69e23b0246e2aed7
KemalBekir <95451696+KemalBekir@users.noreply.github.com>
2022-07-24 21:40:28 +0100

    added basic form to register component

--- client/src/api/User.js
+++ client/src/api/User.js
@@ -1,3 +1,5 @@
+
+
 export async function loginUser({email, password}){
     return fetch('http://localhost:3030/users/login', {
         method: 'POST',
@@ -6,6 +8,16 @@ export async function loginUser({email, password}){
         },
         body: JSON.stringify({email, password})
     })
-    .then(data => data.json())
+    .then(data => data.json());
 }
 
+
+export async function registerUser({username,email, password, tel}) {
+    return fetch('http://localhost:3030/users/register',{
+        method: 'POST',
+        headers: {
+            'Conntent-Type': 'application/json'
+        },
+        body: JSON.stringify({username, email, password, tel})
+    }).then(data => data.json());
+}
--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <h2>Register Page</h2>
+    <section className='register-section'>
+      <form>
+        <label>Username:</label>
+        <input type="text" placeholder='Enter your Username'></input>
+        <label>Email:</label>
+        <input type="email" placeholder='Enter your Email'></input>
+        <label>Password:</label>
+        <input type="password" placeholder='Enter your Password'></input>
+        <label>Tel:</label>
+        <input type="number"></input>
+      </form>
+    </section>
   )
 }
 
